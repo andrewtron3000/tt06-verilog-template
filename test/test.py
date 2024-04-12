edcd1a05dcd297ab4ba0fa6b0023b50abbefbcd5
andrewtron3000 <andrewtron3000@users.noreply.github.com>
2024-04-11 21:32:52 -0400

    Add initial test of UART output.

--- test/test.py
+++ test/test.py
@@ -4,29 +4,44 @@
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
+from cocotbext.uart import UartSink
 
 @cocotb.test()
 async def test_project(dut):
   dut._log.info("Start")
   
-  # Our example module doesn't use clock and reset, but we show how to use them here anyway.
-  clock = Clock(dut.clk, 10, units="us")
+  # Define the clock at 50 MHz
+  clock = Clock(dut.clk, 20, units="ns")
   cocotb.start_soon(clock.start())
+
+  # Set some initial values.  Start disabled.
+  dut.ena.value = 0
+  dut.ui_in.value = 0x80
+  dut.uio_in.value = 0
+  uart_sink = UartSink(dut.uo_out[4], baud=115200, bits=8)
 
   # Reset
   dut._log.info("Reset")
-  dut.ena.value = 1
-  dut.ui_in.value = 0
-  dut.uio_in.value = 0
   dut.rst_n.value = 0
-  await ClockCycles(dut.clk, 10)
+  await ClockCycles(dut.clk, 20)
   dut.rst_n.value = 1
 
-  # Set the input values, wait one clock cycle, and check the output
+  # Set the input values, wait clock cycle, and check the output
   dut._log.info("Test")
-  dut.ui_in.value = 20
-  dut.uio_in.value = 30
+  dut.ena.value = 1
 
-  await ClockCycles(dut.clk, 1)
+  # Await UART data to come back
+  # Input  0x80:  _10000000_
+  # Output 0xC0:   11000000           
+  data = await uart_sink.recv(1)
+  assert data == 0xC0
 
-  assert dut.uo_out.value == 50
+  # Input  0xC0:  _11000000_
+  # Output 0xA0:   10100000           
+  data = await uart_sink.recv(1)
+  assert data == 0xA0
+
+  # Input  0xA0:  _10100000_
+  # Output 0xB0:   10110000           
+  data = await uart_sink.recv(1)
+  assert data == 0xB0
